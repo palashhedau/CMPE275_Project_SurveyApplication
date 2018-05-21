dd4cfa718b09e815935add958c9fdb9671bafb3b
Ubuntu <ubuntu@ip-172-31-28-49.us-west-1.compute.internal>
2018-05-21 06:43:41 +0000

    changing

--- back-end/src/main/java/com/tools/controller/SurveyController.java
+++ back-end/src/main/java/com/tools/controller/SurveyController.java
@@ -266,7 +266,7 @@ public class SurveyController {
 	@RequestMapping(path="/question-stats/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> getQuestionDetails(@PathVariable int id,  HttpSession session ) {
 		if(session.getAttribute("email") != null) {
-			return new ResponseEntity(surveyService.getQuestionStats(id), HttpStatus.OK);
+			return new ResponseEntity(surveyService.getQuestionStats(id,3), HttpStatus.OK);
 		}else {
 			return new ResponseEntity(new Response(404, "Not Authorized to get the survey"), HttpStatus.UNAUTHORIZED);
 		}	
